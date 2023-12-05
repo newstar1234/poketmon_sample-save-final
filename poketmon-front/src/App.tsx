e932ad5d93c14134440cfb5f375805f48b3bb26b
newstar <maori36@naver.com>
2023-12-05 16:46:18 +0900

    23.12.05

--- poketmon-front/src/App.tsx
+++ poketmon-front/src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { MAIN_PATH, RESULT_PATH, SAVE_PATH } from './constants';
+import Main from './views/main';
+import Save from './views/save';
+import Result from './views/result';
+
+// description : main - '/' => MAIN_PATH //
+// description : result - '/result => RESULT_PATH //
+// description : save - '/save' => SAVE_PATH //
 
 function App() {
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path={MAIN_PATH()} element={<Main/>} />
+        <Route path={SAVE_PATH()} element={<Save/>} />
+        <Route path={RESULT_PATH()} element={<Result/>} />
+      </Routes>
+    </Router>
   );
 }
 
